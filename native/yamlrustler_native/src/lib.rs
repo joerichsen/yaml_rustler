2af0db3f789d7c48ca757756a6e089bae6880465
Jørgen Orehøj Erichsen <joe@erichsen.net>
2024-07-27 21:55:03 +0200

    More tests and more advanced yaml support

--- native/yamlrustler_native/src/lib.rs
+++ native/yamlrustler_native/src/lib.rs
@@ -1,15 +1,48 @@
 use rustler::{Encoder, Env, Term, NifResult, types::atom::ok, types::tuple::make_tuple};
 use yaml_rust2::{YamlLoader, Yaml};
-use std::collections::HashMap;
 
 #[rustler::nif]
 fn parse<'a>(env: Env<'a>, input: String) -> NifResult<Term<'a>> {
     match YamlLoader::load_from_str(&input) {
         Ok(docs) => {
-            let result = convert_yaml_to_term(env, &docs[0]);
+            let resolved = resolve_aliases(&docs[0], &docs);
+            let result = convert_yaml_to_term(env, &resolved);
             Ok(make_tuple(env, &[ok().encode(env), result]))
         },
         Err(e) => Err(rustler::Error::Term(Box::new(format!("YAML parsing error: {}", e)))),
+    }
+}
+
+fn resolve_aliases<'a>(yaml: &'a Yaml, docs: &'a [Yaml]) -> Yaml {
+    match yaml {
+        Yaml::Hash(h) => {
+            let mut new_hash = yaml_rust2::yaml::Hash::new();
+            for (k, v) in h {
+                let new_k = resolve_aliases(k, docs);
+                let new_v = resolve_aliases(v, docs);
+                if new_k == Yaml::String("<<".to_string()) {
+                    if let Yaml::Hash(merge_hash) = new_v {
+                        for (merge_k, merge_v) in merge_hash {
+                            if !new_hash.contains_key(&merge_k) {
+                                new_hash.insert(merge_k.clone(), merge_v.clone());
+                            }
+                        }
+                    }
+                } else {
+                    new_hash.insert(new_k, new_v);
+                }
+            }
+            Yaml::Hash(new_hash)
+        },
+        Yaml::Array(a) => Yaml::Array(a.iter().map(|v| resolve_aliases(v, docs)).collect()),
+        Yaml::Alias(i) => {
+            if let Some(alias_value) = docs.get(*i) {
+                resolve_aliases(alias_value, docs)
+            } else {
+                Yaml::Null
+            }
+        },
+        _ => yaml.clone(),
     }
 }
 
@@ -21,14 +54,14 @@ fn convert_yaml_to_term<'a>(env: Env<'a>, yaml: &Yaml) -> Term<'a> {
         Yaml::Boolean(b) => b.encode(env),
         Yaml::Array(a) => a.iter().map(|v| convert_yaml_to_term(env, v)).collect::<Vec<Term>>().encode(env),
         Yaml::Hash(h) => {
-            let map: HashMap<_, _> = h.iter()
+            let map: std::collections::HashMap<_, _> = h.iter()
                 .map(|(k, v)| (convert_yaml_to_term(env, k), convert_yaml_to_term(env, v)))
                 .collect();
             map.encode(env)
         },
-        Yaml::Alias(_) => "<<alias>>".encode(env),
-        Yaml::Null => ().encode(env),
+        Yaml::Null => rustler::types::atom::nil().encode(env),
         Yaml::BadValue => "<<bad_value>>".encode(env),
+        Yaml::Alias(_) => rustler::types::atom::nil().encode(env), // This should not happen after resolution
     }
 }
 
--- test/yaml_rustler_test.exs
+++ test/yaml_rustler_test.exs
@@ -41,4 +41,132 @@ defmodule YamlRustlerTest do
     yaml = "invalid: : yaml"
     assert {:error, "YAML parsing error: " <> _} = YamlRustler.parse(yaml)
   end
+
+  test "parses strings" do
+    yaml = """
+    single_quoted: 'This is a single-quoted string'
+    double_quoted: "This is a double-quoted string"
+    unquoted: This is an unquoted string
+    multiline: |
+      This is a
+      multiline string
+    folded: >
+      This is a folded
+      string
+    """
+
+    assert {:ok, result} = YamlRustler.parse(yaml)
+    assert result["single_quoted"] == "This is a single-quoted string"
+    assert result["double_quoted"] == "This is a double-quoted string"
+    assert result["unquoted"] == "This is an unquoted string"
+    assert result["multiline"] == "This is a\nmultiline string\n"
+    assert result["folded"] == "This is a folded string\n"
+  end
+
+  test "parses numbers" do
+    yaml = """
+    integer: 42
+    negative: -17
+    float: 3.14
+    scientific: 6.02e23
+    """
+
+    assert {:ok, result} = YamlRustler.parse(yaml)
+    assert result["integer"] == 42
+    assert result["negative"] == -17
+    assert result["float"] == 3.14
+    assert result["scientific"] == 6.02e23
+  end
+
+  test "parses booleans and null" do
+    yaml = """
+    true_value: true
+    false_value: false
+    null_value: null
+    """
+
+    assert {:ok, result} = YamlRustler.parse(yaml)
+    assert result["true_value"] == true
+    assert result["false_value"] == false
+    assert result["null_value"] == nil
+  end
+
+  test "parses lists" do
+    yaml = """
+    simple_list:
+      - item1
+      - item2
+      - item3
+    nested_list:
+      - - nested1
+        - nested2
+      - - nested3
+        - nested4
+    """
+
+    assert {:ok, result} = YamlRustler.parse(yaml)
+    assert result["simple_list"] == ["item1", "item2", "item3"]
+    assert result["nested_list"] == [["nested1", "nested2"], ["nested3", "nested4"]]
+  end
+
+  test "parses maps" do
+    yaml = """
+    simple_map:
+      key1: value1
+      key2: value2
+    nested_map:
+      outer1:
+        inner1: value1
+        inner2: value2
+      outer2:
+        inner3: value3
+    """
+
+    assert {:ok, result} = YamlRustler.parse(yaml)
+    assert result["simple_map"] == %{"key1" => "value1", "key2" => "value2"}
+
+    assert result["nested_map"] == %{
+             "outer1" => %{"inner1" => "value1", "inner2" => "value2"},
+             "outer2" => %{"inner3" => "value3"}
+           }
+  end
+
+  test "parses anchors and aliases" do
+    yaml = """
+    defaults: &defaults
+      adapter: postgres
+      host: localhost
+
+    development:
+      <<: *defaults
+      database: myapp_development
+
+    test:
+      <<: *defaults
+      database: myapp_test
+    """
+
+    assert {:ok, result} = YamlRustler.parse(yaml)
+    assert result["development"]["adapter"] == "postgres"
+    assert result["development"]["host"] == "localhost"
+    assert result["development"]["database"] == "myapp_development"
+    assert result["test"]["adapter"] == "postgres"
+    assert result["test"]["host"] == "localhost"
+    assert result["test"]["database"] == "myapp_test"
+  end
+
+  test "parses tags" do
+    yaml = """
+    date: !!timestamp 2023-04-13
+    set: !!set
+      ? item1
+      ? item2
+      ? item3
+    """
+
+    assert {:ok, result} = YamlRustler.parse(yaml)
+    assert result["date"] == "2023-04-13"
+    assert is_map(result["set"])
+    assert Map.keys(result["set"]) |> Enum.sort() == ["item1", "item2", "item3"]
+  end
 end
