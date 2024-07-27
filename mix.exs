c4887b277024887a74ef45fb03474e96ef645735
Jørgen Orehøj Erichsen <joe@erichsen.net>
2024-07-27 21:55:19 +0200

    Release 0.1.2 with more advanced yaml support

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule YamlRustler.MixProject do
   def project do
     [
       app: :yaml_rustler,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
